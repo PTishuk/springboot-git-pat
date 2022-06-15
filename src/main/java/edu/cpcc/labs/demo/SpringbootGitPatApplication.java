1cd0b144fe1b51f1dc33cbe091eea25222145160
ntsishu0 <54260758+ntsishu0@users.noreply.github.com>
2022-06-15 14:30:27 -0400

    Initial version.

---
+++ src/main/java/edu/cpcc/labs/demo/SpringbootGitPatApplication.java
@@ -0,0 +1,13 @@
+package edu.cpcc.labs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitPatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitPatApplication.class, args);
+	}
+
+}
